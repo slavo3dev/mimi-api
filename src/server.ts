356cabc5045661e2ada571b67bf515a04df28e1b
SdZika <markozivkovic1982@gmail.com>
2025-09-17 19:35:50 +0200

    test node server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World MimiKa Server');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running: http://localhost:${PORT} (env PORT=${process.env.PORT ?? 3000})`);
+});
